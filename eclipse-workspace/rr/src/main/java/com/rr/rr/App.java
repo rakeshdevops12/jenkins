6cabbfe834aab17225e4a23ee096fab45b9d1547
Rakesh <Rakesh>
2017-10-16 13:13:49 +0530

    eclipse files

---
+++ eclipse-workspace/rr/src/main/java/com/rr/rr/App.java
@@ -0,0 +1,13 @@
+package com.rr.rr;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
